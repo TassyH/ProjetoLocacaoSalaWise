d241052102c23c2a8b198d76c40e8dcd85e8f411
Tassy <tassy.lazame42@gmail.com>
2020-01-22 16:22:15 -0800

    Primeiro commit do projeto

---
+++ app/src/main/java/com/example/locacaodesalas/Ui/CadastroUsuarioActivity.java
@@ -0,0 +1,16 @@
+package com.example.locacaodesalas.Ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.locacaodesalas.R;
+
+public class CadastroUsuarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_usuario);
+    }
+}
