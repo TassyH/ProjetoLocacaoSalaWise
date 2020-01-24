13933b020a6bd984ca0fac51a4b8a6845bfb0347
Tassy <tassy.lazame42@gmail.com>
2020-01-24 03:35:40 -0300

    commit casa 24-01-20 magrugada

---
+++ app/src/main/java/com/example/locacaodesalas/Ui/CadastroReservaActivity.java
@@ -0,0 +1,14 @@
+package com.example.locacaodesalas.Ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CadastroReservaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_reserva);
+    }
+}
